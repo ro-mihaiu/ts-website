d42586348b69e12199ed10e82e8ef383ac4b0054
talhamolla777 <talhamolla777@gmail.com>
2026-08-24 14:58:13 +0600

    510-dn.tsx

--- src/data/farms/510-dn.tsx
+++ src/data/farms/510-dn.tsx
@@ -5,7 +5,7 @@ export const farm: FarmData = {
   title: "1,530 In 5 Minutes! BEST New RAID Farm in Minecraft 1.21+",
   farmType: "Raid",
   description: "Comprehensive Minecraft Java tutorial and build blueprint for 1,530 In 5 Minutes! BEST New RAID Farm in Minecraft 1.21+. Designed by TheySix for high efficiency, reliability, and ease of construction in survival mode.",
-  worldDownloadUrl: "link",
+  worldDownloadUrl: "https://www.mediafire.com/file/0iq9dkm0jsyrw6z/TheySix+-+RXP+RF+V2.rar/file",
   youtubeUrl: "https://www.youtube.com/watch?v=pfVYmMo1dk4",
   version: "1.21++",
   rates: "1.5k In 5 Min",
