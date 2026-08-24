c6ca3233dfe82deb729565f4c3368812526c841d
talhamolla777 <talhamolla777@gmail.com>
2026-08-24 15:02:10 +0600

    513-dn.tsx

--- src/data/farms/513-dn.tsx
+++ src/data/farms/513-dn.tsx
@@ -5,7 +5,7 @@ export const farm: FarmData = {
   title: "39 Levels in 30 Seconds! INSANE XP Farm for Minecraft 1.21/26.1+!",
   farmType: "XP",
   description: "Comprehensive Minecraft Java tutorial and build blueprint for 39 Levels in 30 Seconds! INSANE XP Farm for Minecraft 1.21/26.1+!. Designed by TheySix for high efficiency, reliability, and ease of construction in survival mode.",
-  worldDownloadUrl: "link",
+  worldDownloadUrl: "https://www.mediafire.com/file/o64sbd10pa315vq/TheySix+-+XE+XP+V1.rar/file",
   youtubeUrl: "",
   version: "1.21/26.1++",
   rates: "39 Levels",
