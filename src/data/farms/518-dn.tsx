76516815d9135e4f45b3d0a5f8898481e0dcbf64
talhamolla777 <talhamolla777@gmail.com>
2026-08-24 16:36:27 +0600

    518-dn.tsx

--- src/data/farms/518-dn.tsx
+++ src/data/farms/518-dn.tsx
@@ -5,7 +5,7 @@ export const farm: FarmData = {
   title: "NEW INFINITE Mob Farm In Minecraft Bedrock +1.21/26.2!",
   farmType: "Mob",
   description: "Comprehensive Minecraft Java tutorial and build blueprint for NEW INFINITE Mob Farm In Minecraft Bedrock +1.21/26.2!. Designed by TheySix for high efficiency, reliability, and ease of construction in survival mode.",
-  worldDownloadUrl: "link",
+  worldDownloadUrl: "https://www.mediafire.com/file/4shcxw2d3csbup2/TheySix+-+IFCX+V1C.rar/file",
   youtubeUrl: "https://www.youtube.com/watch?v=2e2mHOGF5qA",
   version: "1.21+",
   rates: "4k+ Drops/hr",
